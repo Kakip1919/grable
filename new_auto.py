204a36256181881417c6ad9414892f0a8c161478
Kakip1919 <invest9me@yahoo.co.jp>
2021-12-29 17:11:52 +0900

    2nd update

--- new_auto.py
+++ new_auto.py
@@ -3,22 +3,50 @@ import requests
 import pyautogui as gui
 import random
 from time import sleep
-import os
+
 size = "Size(width=1920, height=1080)"
+
+
+# ショートカットキーで削除
+def close_window():
+    import pyautogui
+
+    pyautogui.keyDown('ctrl')
+    pyautogui.press('w')
+    pyautogui.keyUp('ctrl')
+    return
+
+
+# x, yは下限を返し、widthとheightは最終的な上限を返す
+def pos(img):
+    x, y = gui.locateCenterOnScreen(img, confidence=0.7)
+    left, top, max_x, max_y = gui.locateOnScreen(img, confidence=0.7)
+    x -= max_x / 2
+    y -= max_y / 2
+    max_x += x
+    max_y += y
+
+    return int(x), int(y+5), int(max_x), int(max_y)
+
+
+def random_pos(img):
+    return random.randrange(pos(img)[0], pos(img)[2]), random.randrange(pos(img)[1], pos(img)[3])
 
 
 def partic():
     i = 0
     webbrowser.open("http://game.granbluefantasy.jp/#quest/assist")
     while True:
-    
+
         i += 1
         if str(gui.size()) == size:
             sleep(5)
-            gui.moveTo(random.randrange(674, 795), random.randrange(276, 310), random.uniform(0.8, 1.3))
+            gui.moveTo(random_pos("id_insert.png")[0], random_pos("id_insert.png")[1]
+                       , random.uniform(0.8, 1.3))
             sleep(random.uniform(0.6, 1))
             gui.click()
-            gui.moveTo(random.randrange(398, 542), random.randrange(551, 565), random.uniform(0.8, 1.3))
+
+            gui.moveTo(gui.locateCenterOnScreen('input.png'))
             gui.click()
             sleep(random.uniform(0.3, 1))
 
@@ -37,7 +65,8 @@ def partic():
                 res = res.json()["war_code"]
                 gui.write(res)
 
-            gui.moveTo(random.randrange(609, 724), random.randrange(541, 560), random.uniform(0.8, 1.3))
+            gui.moveTo(random_pos("assign.png")[0], random_pos("assign.png")[1]
+                       , random.uniform(0.8, 1.3))
             gui.click()
             sleep(random.uniform(0.5, 1))
 
@@ -59,22 +88,29 @@ def partic():
                 sleep(random.uniform(0.5, 1.3))
                 gui.click()
 
-            while True:
-                if gui.locateOnScreen('agnis5.png', confidence=0.9):
-                    position = gui.locateOnScreen('agnis5.png', confidence=0.9)
-                    break
-                elif gui.locateOnScreen('agnis4.png', confidence=0.9):
-                    position = gui.locateOnScreen('agnis4.png', confidence=0.9)
-                    break
-                elif gui.locateOnScreen('shiva.png', confidence=0.9):
-                    position = gui.locateOnScreen('shiva.png', confidence=0.9)
-                    break
-                else:
-                    gui.scroll(-600)
+            if gui.locateOnScreen('supporter.png', confidence=0.7):
+                while True:
+                    if gui.locateOnScreen('agnis5.png', confidence=0.9):
+                        gui.moveTo(random_pos("agnis5.png")[0], random_pos("agnis5.png")[1],
+                                   random.uniform(0.8, 1.3))
+                        break
+                    elif gui.locateOnScreen('agnis4.png', confidence=0.9):
+                        gui.moveTo(random_pos("agnis4.png")[0], random_pos("agnis4.png")[1],
+                                   random.uniform(0.8, 1.3))
+                        break
+                    elif gui.locateOnScreen('shiva.png', confidence=0.9):
+                        gui.moveTo(random_pos("shiva.png")[0], random_pos("shiva.png")[1],
+                                   random.uniform(0.8, 1.3))
+                        break
+                    else:
+                        gui.scroll(-600)
+            else:
+                close_window()
 
-            gui.click(position)
-            gui.moveTo(random.randrange(591, 705), random.randrange(740, 750), random.uniform(0.8, 1.3))
+            gui.click()
             sleep(random.uniform(0.3, 1))
+            gui.moveTo(random_pos("OK.png")[0], random_pos("OK.png")[1],
+                       random.uniform(0.8, 1.3))
             gui.click()
 
             while True:
@@ -90,12 +126,15 @@ def partic():
 
             while True:
                 if gui.locateOnScreen('result.png', confidence=0.5):
+                    close_window()
                     webbrowser.open("http://game.granbluefantasy.jp/#quest/assist")
                     break
                 elif gui.locateOnScreen('battle_completed.png', confidence=0.5):
+                    close_window()
                     webbrowser.open("http://game.granbluefantasy.jp/#quest/assist")
                     break
                 else:
                     sleep(5)
+
 
 partic()
--- test.py
+++ test.py
@@ -1,7 +1,13 @@
-import pyautogui
+import pyautogui as gui
 from time import sleep
+
 while True:
-    x,y = pyautogui.position()
+    x, y = gui.locateCenterOnScreen('OK.png', confidence=0.8)
+    lft, t, w, h = gui.locateOnScreen("OK.png", confidence=0.8)
+    print(int(w / 2), int(h / 2))
     print(x, y)
     sleep(1)
+
+
+
 
